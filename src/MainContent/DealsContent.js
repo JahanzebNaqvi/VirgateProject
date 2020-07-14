e159f959deaf55ee332ee0d2a2534e0fc2cb3572
JahanzebNaqvi <j.naqvi70@yahoo.com>
2020-07-15 00:04:57 +0500

    Completed Deal Content Div

--- src/MainContent/DealsContent.js
+++ src/MainContent/DealsContent.js
@@ -5,15 +5,85 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import image1 from '../Images/bunglow1.jpg'
+
 class DealsContent extends React.Component {
     render() {
+        const {image, title, address,YearOfCompletion,LandTenure,SiteArea,NLA9,Carparklots,Components,MajorTenants } = this.props;
         return (
-            <Container className="">
+            <Container className="card DarkNavy poppins-bold">
                 <Row>
-                    <img src={image1} className="DealsImage"/>
+                    <img src={image} className="DealsImage" />
                 </Row>
-                <Row>
+                <Row className="DealContentCard">
+                    <Col md="12">
+                    <Row className="DealTitle">
+                        <h4>{title}</h4>
+                    </Row>
+                    <Row className="DealAddress">
+                        <p>{address}</p>
+                    </Row>
+                    <Row className="DealNavyLine">
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Year Of Completion</label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{YearOfCompletion}</label>
+                       </Col>
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Land Tenure</label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{LandTenure}</label>
+                       </Col>
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Site Area (sqft)</label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{SiteArea}</label>
+                       </Col>
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>NLA 9 (sqft)</label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{NLA9}</label>
+                       </Col>
+                    </Row>
+                    <Row className="DealNavyLine">
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Carpark lots </label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{Carparklots}</label>
+                       </Col>
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Components </label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{Components}</label>
+                       </Col>
+                    </Row>
+                    <Row className="Attribute">
+                        <Col md="6" className="AttributeName">
+                       <label>Major Tenants</label>
+                       </Col>
+                       <Col md="6" className="AttributeValue">
+                       <label>{MajorTenants}</label>
+                       </Col>
+                    </Row>
+              
+                    </Col>
                 </Row>
             </Container>
         )
--- src/MainContent/index.js
+++ src/MainContent/index.js
@@ -5,6 +5,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import DealContent from './DealsContent';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import image1 from '../Images/bunglow1.jpg'
+import image3 from '../Images/bunglow2.jpg'
+import image2 from '../Images/bunglow3.jpg'
 
 class MainContent extends React.Component {
     render() {
@@ -23,11 +26,46 @@ class MainContent extends React.Component {
                 </Row>
                 <Row className="DealContent">
                     <Col md="4">
-                        <DealContent />
+                        <DealContent
+                            image={image1}
+                            title="Robinson77"
+                            address="77 Robinon Road, Shenton Way-Robinson Rd-Cecil St, Singapore"
+                            YearOfCompletion="2019"
+                            LandTenure="99 Years"
+                            SiteArea="27,281"
+                            NLA9="3,700,000"
+                            Carparklots="900"
+                            Components="Office, Retail, Hotel"
+                            MajorTenants="CITI Bank, Allianz Mitsui"
+                        />
                     </Col>
                     <Col md="4">
+                        <DealContent
+                            image={image2}
+                            title="Marina One East Tower"
+                            address="7 Straits View, Singapore 018936 Singapore"
+                            YearOfCompletion="2019"
+                            LandTenure="99 Years"
+                            SiteArea="27,281"
+                            NLA9="3,700,000"
+                            Carparklots="900"
+                            Components="Office, Retail, Hotel"
+                            MajorTenants="CITI Bank, Allianz Mitsui"
+                        />
                     </Col>
                     <Col md="4">
+                        <DealContent
+                            image={image3}
+                            title="Mapletree Business City"
+                            address="10 Pasir Panjang Rd, 117438 Singapore"
+                            YearOfCompletion="2019"
+                            LandTenure="99 Years"
+                            SiteArea="27,281"
+                            NLA9="3,700,000"
+                            Carparklots="900"
+                            Components="Office, Retail, Hotel"
+                            MajorTenants="CITI Bank, Allianz Mitsui"
+                        />
 
                     </Col>
                 </Row>
