6ddc42ad0cf71581e4ccca155982ac273cac6785
JahanzebNaqvi <j.naqvi70@yahoo.com>
2020-07-15 23:03:38 +0500

    Deployment files

---
+++ build/precache-manifest.ad849d8d40545f4f6dd2b2a949c34948.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93f5ef2d0be0b4bead4eb56bf3b4026",
+    "url": "/VirgateProject/index.html"
+  },
+  {
+    "revision": "5a30d2f87414779568ca",
+    "url": "/VirgateProject/static/css/main.825ab662.chunk.css"
+  },
+  {
+    "revision": "d27f034f08a09b98ed95",
+    "url": "/VirgateProject/static/js/2.cef69003.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/VirgateProject/static/js/2.cef69003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a30d2f87414779568ca",
+    "url": "/VirgateProject/static/js/main.2a3af727.chunk.js"
+  },
+  {
+    "revision": "1769dda4c446969290e3",
+    "url": "/VirgateProject/static/js/runtime-main.6e87226f.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/VirgateProject/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/VirgateProject/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/VirgateProject/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/VirgateProject/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/VirgateProject/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/VirgateProject/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/VirgateProject/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "1e6d95e7af4f3076c36e1ea4f548939c",
+    "url": "/VirgateProject/static/media/backgroundImage.1e6d95e7.png"
+  },
+  {
+    "revision": "8837705a4ab97c81b9cd68930c5dcffb",
+    "url": "/VirgateProject/static/media/bunglow1.8837705a.jpg"
+  },
+  {
+    "revision": "80279a88080147fda9b1149e46ddc519",
+    "url": "/VirgateProject/static/media/bunglow2.80279a88.jpg"
+  },
+  {
+    "revision": "942110ef7efe7cbb1422f2d3adbc1838",
+    "url": "/VirgateProject/static/media/bunglow3.942110ef.jpg"
+  }
+]);
